8d340156bec95f96df9a24c5e02cdc88eedd6fb0
mohansharma916 <mohansharma916@gmail.com>
2024-02-24 16:38:33 +0530

    adding payment confirmation

--- src/payment-gateway/payment-gateway.service.ts
+++ src/payment-gateway/payment-gateway.service.ts
@@ -19,7 +19,7 @@ export class PaymentGatewayService {
         merchantUserId: 'MUID' + '1234',
         name: 'RAJU',
         amount: 1 * 100,
-        redirectUrl: `https://planetseraapi.planetsera.com/api/v1/status/${merchantTransactionId}`,
+        redirectUrl: `https://planetsera.com/payment-status/${merchantTransactionId}`,
         email: 'mohan@gmail.com',
         redirectMode: 'POST',
 
